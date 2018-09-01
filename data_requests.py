26a4f654e58a4e05b2a40d9a6894f8d738942c85
janikgar <janikgar@gmail.com>
2018-09-01 17:05:04 -0500

    request works, but not auth

--- data_requests.py
+++ data_requests.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 
 # import requests
-# from pydoc import help
-from googleapiclient.discovery import build
-from oauth2client import file, client, tools
-from httplib2 import Http
+from pydoc import help
+import google.auth
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
 
 SPREADSHEET_ID = "1wbnG31Z5QBm2fuyzZOY9XkSij0EERtX92wEHq9LbPiI"
-
-SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly"
-
-GID = {"transactions":"1040087283",
-       "categories":"886661001",
-       "balance_history":"1324484343"}
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+URL = "https://sheets.googleapis.com/v4/spreadsheets/" + SPREADSHEET_ID
+# GID = {"transactions":"1040087283",
+#        "categories":"886661001",
+#        "balance_history":"1324484343"}
 
 def main():
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    # credentials, project = google.auth.default()
+    credentials = service_account.Credentials.from_service_account_file('credentials.json').with_scopes(SCOPES)
+    authed_session = AuthorizedSession(credentials)
+    # print(scope)
+    response = authed_session.get(URL)
+    print(response.text)
 
-    result = service.spreadsheets()
-
-# sheet = "https://docs.google.com/spreadsheets/d/1wbnG31Z5QBm2fuyzZOY9XkSij0EERtX92wEHq9LbPiI/export?format=csv&gid="
-
-# transactions = requests.get(sheet+gid['transactions'])
-
-# print(help(transactions.text))
+if __name__ == '__main__':
+    main()
