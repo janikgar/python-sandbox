46996bbac5c42e725de4f9e0d4f673d1673decd4
janikgar <janikgar@gmail.com>
2018-09-01 20:02:56 -0500

    stubbed oauth info

--- data_requests.py
+++ data_requests.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 
-# import requests
 from pydoc import help
-import google.auth
-from google.oauth2 import service_account
+from google.oauth2 import credentials
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+import json
 from google.auth.transport.requests import AuthorizedSession
+from pprint import pprint
 
 SPREADSHEET_ID = "1wbnG31Z5QBm2fuyzZOY9XkSij0EERtX92wEHq9LbPiI"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 URL = "https://sheets.googleapis.com/v4/spreadsheets/" + SPREADSHEET_ID
-# GID = {"transactions":"1040087283",
-#        "categories":"886661001",
-#        "balance_history":"1324484343"}
+AUTH_FILE = json.load(open('oauth2.json'))['installed']
+
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    # credentials, project = google.auth.default()
-    credentials = service_account.Credentials.from_service_account_file('credentials.json').with_scopes(SCOPES)
-    authed_session = AuthorizedSession(credentials)
-    # print(scope)
-    response = authed_session.get(URL)
-    print(response.text)
+    # pprint(AUTH_FILE)
+    client = BackendApplicationClient(client_id=AUTH_FILE['client_id'])
+    oauth = OAuth2Session(client=client)
+    authorization_url = oauth.authorization_url(url=AUTH_FILE['auth_uri'])
+    print(authorization_url)
+    # print(help(oauth.fetch_token))
+    # token = oauth.fetch_token(
+    #     token_url=AUTH_FILE['token_uri'],
+    #     client_id=AUTH_FILE['client_id'],
+    #     client_secret=AUTH_FILE['client_secret']
+    # )
+    # creds = credentials.Credentials(token)
+    # sess = AuthorizedSession(creds)
+    # response = sess.get(URL)
+    # print(response.text)
 
 if __name__ == '__main__':
     main()
