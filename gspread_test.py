6bb31290e56ef24a5f50cc1ccbab9d0acbf9f403
janikgar <janikgar@gmail.com>
2018-09-03 18:40:39 -0500

    started user-based OAuth token process

--- gspread_test.py
+++ gspread_test.py
@@ -5,9 +5,12 @@ from pprint import pprint
 from pydoc import help
 import json
 
+from google_auth_oauthlib import flow
+
 from google.auth import credentials
 from oauthlib.oauth2 import BackendApplicationClient
 import requests_oauthlib
+import requests
 # from google.oauth2 import service_account
 # from oauth2client.service_account import ServiceAccountCredentials
 # import oauth2client
@@ -26,16 +29,10 @@ def parse_google_auth(file):
   This function requires a JSON file for a specific Google OAuth user.
   This can be received from the Google Cloud Console for the linked project.
   '''
-  jsonfile = json.load(open(AUTH_FILE))
-  json_installed = jsonfile['installed']
-  # pprint(json_installed)
-  client_id = json_installed['client_id']
-  client_secret = json_installed['client_secret']
-  redirect_uri = 'http://localhost'
-  authorization_base_url = json_installed['auth_uri']
-  token_url = json_installed['token_uri']
-  sess = requests_oauthlib.OAuth2Session(client_id=client_id, scope = SCOPES, redirect_uri=redirect_uri)
-  pprint(sess.access_token)
+  auth_flow = flow.Flow.from_client_secrets_file(file, scopes=SCOPES)
+  auth_flow.redirect_uri = 'http://localhost'
+  auth_url, state = auth_flow.authorization_url(access_type='offline', include_granted_scopes='true')
+  print(auth_url)
 
 parse_google_auth(AUTH_FILE)
 # pprint(help(BackendApplicationClient))
